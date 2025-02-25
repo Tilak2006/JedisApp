b3baca9b60929b910e44d02bff73f4be8076359f
Tilak <tilakj0108@gmail.com>
2025-02-25 11:03:48 +0530

    Capital L fix

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
-import "./login.css";
+import "./Login.css";
 
 const Login = () => {
   const navigate = useNavigate();
